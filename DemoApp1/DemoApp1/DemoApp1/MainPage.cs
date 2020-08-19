246511e6abaccbd00a2ea8f3880b11638a6fce17
Prasanna Sriram <pras306@gmail.com>
2020-08-19 02:22:10 -0400

    Initial Commit

---
+++ DemoApp1/DemoApp1/DemoApp1/MainPage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace DemoApp1
+{
+    public class MainPage: ContentPage
+    {
+        Entry phoneNumberText;
+        Button btnTranslate;
+        Button btnCall;
+        string translatedNumber = string.Empty;
+
+        public MainPage()
+        {
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+
+            panel.Children.Add(btnTranslate = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(btnCall = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            });
+
+            btnTranslate.Clicked += BtnTranslate_Clicked;
+            btnCall.Clicked += BtnCall_Clicked;
+            this.Content = panel;
+        }
+
+        private async void BtnCall_Clicked(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"
+                ))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing is not supported", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed", "OK");
+                }
+
+            }
+        }
+
+        private void BtnTranslate_Clicked(object sender, EventArgs e)
+        {
+            translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                btnCall.IsEnabled = true;
+                btnCall.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                btnCall.IsEnabled = false;
+                btnCall.Text = "Call";
+            }
+        }
+    }
+}
---
+++ DemoApp1/DemoApp1/DemoApp1/PhonewordTranslator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoApp1
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string txt)
+        {
+            string retVal = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(txt)) return null;
+
+            string letters = txt.ToUpper();
+
+            foreach(var letter in letters)
+            {
+                if ("0123456789-".Contains(letter))
+                {
+                    retVal += letter;
+                }
+                else
+                {
+                    var check = numPairs.Find(x => x.Contains(letter));
+                    if(check != null)
+                    {
+                        retVal += Convert.ToString(numPairs.IndexOf(check) + 2);
+                    }
+                }
+            }
+
+            return retVal;
+        }
+
+        static List<string> numPairs = new List<string>{ "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+    }
+}
